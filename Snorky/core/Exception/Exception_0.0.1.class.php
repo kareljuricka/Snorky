081a78bbab5d99703e31baad1f8d3f17707b05cd
speop <david.buchta@hotmail.com>
2014-07-02 21:33:05 +0200

    added exception of syntax error

--- Snorky/core/Exception/Exception_0.0.1.class.php
+++ Snorky/core/Exception/Exception_0.0.1.class.php
@@ -52,3 +52,15 @@ class LexError extends Exception{
     }
 }
 
+/**************************************************
+ *  Parser exceptions
+ **************************************************/
+class SyntaxError extends Exception{
+   
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
+
+}
