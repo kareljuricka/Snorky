6c7b66471a3cb90dca4ccbbcd63663816bf12e51
Karel Juricka <kapa@loveart.cz>
2014-07-04 10:12:29 +0200

    Configurator database access divided into specific protocol

--- Snorky/core/Configurator/Configurator_0.0.1.class.php
+++ Snorky/core/Configurator/Configurator_0.0.1.class.php
@@ -29,15 +29,33 @@ class Configurator {
 		return simplexml_load_file($this->dir . "/" . $configFile);
 	}
 
-	public function getDatabaseData() {
-		return $this->config->Database;
+	public function getUserDatabaseData() {
+		$dbData["driver"] = $this->config->Database->Driver;
+		$dbData["database"] = $this->config->Database->Database;
+		$dbData["server"] = $this->config->Database->Server;
+		$dbData["charset"] = $this->config->Database->Charset;
+		$dbData["login"] = $this->config->Database->User->Login;
+		$dbData["password"] = $this->config->Database->User->Password;
+
+		return $dbData;
+	}
+
+	public function getAdminDatabaseData() {
+		$dbData["driver"] = $this->config->Database->Driver;
+		$dbData["database"] = $this->config->Database->Database;
+		$dbData["server"] = $this->config->Database->Server;
+		$dbData["charset"] = $this->config->Database->Charset;
+		$dbData["login"] = $this->config->Database->Admin->Login;
+		$dbData["password"] = $this->config->Database->Admin->Password;
+
+		return $dbData;
 	}
 
 	public function getTemplateDir() {
-		return  $this->dir .  "/" . $this->config->Template->Dir;
+		return $this->dir .  "/" . $this->config->Template->Dir;
 	}
 
 	public function getTemplate($templateName) {
-		return  $this->getTemplateDir() . "/" . $templateName . "." . $this->config->Template->Extension;
+		return $this->getTemplateDir() . "/" . $templateName . "." . $this->config->Template->Extension;
 	}
 }
