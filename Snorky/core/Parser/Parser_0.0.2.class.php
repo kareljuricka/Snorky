544927960ce17e2e8e4e1b24fe4d437a6dd60c1a
speop <david.buchta@hotmail.com>
2015-01-22 12:03:28 +0100

    coomit

---
+++ Snorky/core/Parser/Parser_0.0.2.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Parser go through php code and make replacment array for all registered variable for replacing in tamplate code. For any new plugin is called new istance
+ * of parser.
+ *
+ * @author David
+ */
+class Parser{
+    //put your code here
+    private $replacement = null;
+    
+    public function __construct() {
+        $this->replacement = new ReplacementRegister();
+    }
+    
+    
+    
+    
+}
---
+++ Snorky/core/Templater/Templater_0.0.2.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Snorky;
+/**
+ * Templater is class which is used to start proces of generation cache file for templates. This class is also used as error 404 handler, in the way that
+ * firt try generate cache file if it is possible, if not than include an Error 404 page.
+ *
+ * @author David
+ */
+class Templater{
+    /**
+     * Register for holding instance for objects created and registered in anz plugin or page code for current page template.
+     * 
+     */
+    protected $InstanceRegister = null;
+    private $CFG = null;
+     /**
+      * This constructor is used in classic way when templater is called from core function.
+      */
+    public function __construct($page) {
+        // creating new register for holding objects
+        $this->InstanceRegister = new InstanceRegister();
+        $this->CFG = Configurator::Instance();
+        
+        if($this->CFG == null){
+            //todo: eception throw
+        }
+        
+        $result = new Parser($page);
+        // adding necessary code need for plugin to work 
+        $code = $this->templaterFrame.$result;
+        
+        //creating cahce file for current page template
+        if (!file_put_contents ($this->CFG->GetCacheDir()->$page."_cache_".time().",php",$code)){
+            //todo: exception throw 
+        }
+    }
+}
